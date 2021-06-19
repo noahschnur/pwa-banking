55dcfe87e56869a1ab72d6b7b74ac4cc69c465ae
Noah <noahschnur@gmail.com>
2021-06-19 09:13:24 -0700

    pre-tutor

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://noahschnur:Giants10@cluster0.nlwua.mongodb.net/bankingApp?retryWrites=true&w=majority";
 
 const app = express();
 
